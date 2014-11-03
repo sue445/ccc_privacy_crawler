9463075143ffc5f4c81f6d1a77f0cbfe4e9e1051
sue445 <sue445@sue445.net>
2014-11-03 20:30:55 +0900

    Refactor companies

--- models/company.rb
+++ models/company.rb
@@ -3,12 +3,16 @@
 # Table name: companies
 #
 #  id               :integer          not null, primary key
-#  no               :integer
-#  company_name     :string(255)
-#  receipted_date   :string(255)
+#  no               :integer          not null
+#  company_name     :string(255)      not null
+#  receipted_date   :string(255)      not null
 #  destination_name :text
 #  created_at       :datetime
 #  updated_at       :datetime
+#
+# Indexes
+#
+#  index_companies_on_no  (no) UNIQUE
 #
 
 class Company < ActiveRecord::Base
