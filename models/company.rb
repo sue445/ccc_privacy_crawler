a8fd1229f5e8bc52cacbc8bdc405f12b7d41eb1e
sue445 <sue445@sue445.net>
2014-11-03 18:03:23 +0900

    rake ar:migrate

--- models/company.rb
+++ models/company.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: companies
+#
+#  id               :integer          not null, primary key
+#  no               :integer
+#  company_name     :string(255)
+#  receipted_date   :string(255)
+#  destination_name :text
+#  created_at       :datetime
+#  updated_at       :datetime
+#
+
 class Company < ActiveRecord::Base
 
 end
