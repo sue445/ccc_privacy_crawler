ac29289c9cf8d5587fc57ab5944331a5e1f54104
sue445 <sue445@sue445.net>
2014-11-03 19:35:08 +0900

    tweak spec

--- spec/lib/workers/pdf_crawl_worker_spec.rb
+++ spec/lib/workers/pdf_crawl_worker_spec.rb
@@ -1,7 +1,7 @@
 RSpec.describe PdfCrawlWorker do
   let(:worker){ PdfCrawlWorker.new }
 
-  describe "#parse_pdf" do
+  describe "#parse_ccc_pdf" do
     subject{ worker.parse_ccc_pdf(pdf_file) }
 
     let(:pdf_file){ "#{spec_dir}/data/ccc_privacy_present_companies_20141020.pdf" }
