3c04ae8a67fd8731b5574c4c088937ecd13a1402
sue445 <sue445@sue445.net>
2014-11-03 19:22:15 +0900

    Refactor: move to pdf_crawl_worker

--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -1,10 +1,2 @@
 RSpec.describe Company do
-  describe "#parse_pdf" do
-    subject{ Company.parse_ccc_pdf(pdf_file) }
-
-    let(:pdf_file){ "#{spec_dir}/data/ccc_privacy_present_companies_20141020.pdf" }
-
-    its(:count){ should eq 81 }
-    it{ should all(be_a_company) }
-  end
 end
