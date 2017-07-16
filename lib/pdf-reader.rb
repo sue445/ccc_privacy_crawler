8c32d80ea93348a6f3ccdbd3e79ad14396688090
sue445 <sue445@sue445.net>
2017-07-16 23:54:50 +0900

    Tweak monkey patch

--- lib/pdf-reader.rb
+++ lib/pdf-reader.rb
@@ -1,9 +1,11 @@
 # rubocop:disable Style/FileName
 require "pdf/reader"
 
-class PDF::Reader::PageLayout
+module PdfReaderExt
   # fix rate: 1.05 -> 1.5
   def col_count
     @col_count ||= ((@page_width / @mean_glyph_width) * 1.5).floor
   end
 end
+
+PDF::Reader::PageLayout.prepend(PdfReaderExt)
