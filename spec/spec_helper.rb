e406492e717ddf5e02ecc3c65b5bdd5aa2a599da
sue445 <sue445@sue445.net>
2020-11-17 22:05:53 +0900

    [Resolved] C: Performance/MethodObjectAsBlock: Use block explicitly instead of block-passing a method object.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,7 +5,7 @@ end
 
 RACK_ENV = "test".freeze unless defined?(RACK_ENV)
 require File.expand_path("#{File.dirname(__FILE__)}/../config/boot")
-Dir[File.expand_path("#{File.dirname(__FILE__)}/../app/helpers/**/*.rb")].sort.each(&method(:require))
+Dir[File.expand_path("#{File.dirname(__FILE__)}/../app/helpers/**/*.rb")].sort.each {|r| require r }
 
 require "database_rewinder"
 require "rspec/its"
