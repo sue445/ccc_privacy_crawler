083a8fb3604667264d310a0d99fe4449e4f75d17
sue445 <sue445@sue445.net>
2021-12-14 23:00:09 +0900

    Fix for v2.7.0
    https://github.com/yob/pdf-reader/compare/v2.6.0...df93850#diff-4403a9541bf827d94b4b5fc439ab7cea2cdd821a9e521edb7658bcbe2cc7b144R85

--- lib/pdf-reader.rb
+++ lib/pdf-reader.rb
@@ -4,7 +4,7 @@ module PdfReaderExt
   # fix rate: 1.05 -> 1.5
   # c.f https://github.com/yob/pdf-reader/blob/v2.6.0/lib/pdf/reader/page_layout.rb#L72-L74
   def col_count
-    @col_count ||= ((@page_width / @median_glyph_width) * 1.5).floor
+    @col_count ||= ((page_width / @median_glyph_width) * 1.5).floor
   end
 end
 
