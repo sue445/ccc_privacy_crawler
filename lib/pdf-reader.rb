5dde931421f1e48a43430a5ed0acafe5071b9d93
sue445 <sue445@sue445.net>
2021-12-14 23:02:09 +0900

    Fix comment [ci skip]

--- lib/pdf-reader.rb
+++ lib/pdf-reader.rb
@@ -2,7 +2,7 @@ require "pdf/reader"
 
 module PdfReaderExt
   # fix rate: 1.05 -> 1.5
-  # c.f https://github.com/yob/pdf-reader/blob/v2.6.0/lib/pdf/reader/page_layout.rb#L72-L74
+  # c.f https://github.com/yob/pdf-reader/blob/df93850/lib/pdf/reader/page_layout.rb#L84-L86
   def col_count
     @col_count ||= ((page_width / @median_glyph_width) * 1.5).floor
   end
