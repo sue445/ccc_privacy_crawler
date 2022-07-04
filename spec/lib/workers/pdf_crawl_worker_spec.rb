5810afa7acaf9bcdc68567a210849b10ce73fe2d
sue445 <sue445@sue445.net>
2022-07-04 22:13:37 +0900

    Update pdf

--- spec/lib/workers/pdf_crawl_worker_spec.rb
+++ spec/lib/workers/pdf_crawl_worker_spec.rb
@@ -27,7 +27,7 @@ RSpec.describe PdfCrawlWorker do
   describe "#parse_ccc_pdf" do
     subject { worker.parse_ccc_pdf(pdf_file) }
 
-    let(:pdf_file) { "#{spec_dir}/data/ccc_privacy_present_companies_20141020.pdf" }
+    let(:pdf_file) { "#{spec_dir}/data/ccc_privacy_present_companies_20220701.pdf" }
 
     its(:count) { should eq 81 }
     it { should all(be_a_company(receipted_date: "2014/10/20")) }
